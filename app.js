a69f01f138de10a9c037c81818cbd0e6f0c530ea
Laura Oviedo <lau.oviedo@gmail.com>
2022-05-08 00:13:48 -0300

    header and footer

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require ("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './Public');
+const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.listen(3000,() => {
