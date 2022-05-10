31f96ebbae47ce74c3bc570a719ec76068d7e922
Margarita Gonzalez <margaritagonzalez@MacBook-Air-de-Margarita.local>
2022-05-10 18:14:17 -0300

    creando login

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ app.use(express.static(publicPath));
 
 app.listen(3000,() => {
     console.log("servidor corriendo en el puerto 3000");
-
 });
+
 app.get('/', (req,res) =>{
 res.sendFile(path.resolve(__dirname, './Views/home.html'))
 });
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './Views/login.html'))
+    });
