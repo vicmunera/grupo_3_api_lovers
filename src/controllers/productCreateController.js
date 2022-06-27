2236e0bd8791a613b970bc0ac812570460e4780d
BraianMaynard <Braian_maynard@hotmail.com>
2022-06-27 19:21:22 -0300

    CRUD-POST

--- src/controllers/productCreateController.js
+++ src/controllers/productCreateController.js
@@ -1,20 +1,23 @@
 const path = require('path');
 const fs = require('fs');
 
+
 const controller = {
     index: (req, res) => {
-        let ultProduct = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../Data/productDataBase.json")));
-        res.render("productCreateForm")
+        
+         let productDataBase = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../Data/productDataBase.json')));
+         res.render(path.resolve(__dirname, '../views/productCreateForm'));
     },
 
     create: (req, res) => {
 
-        let ultProduct = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../Data/productDataBase.json")));
+        let productDataBase = JSON.parse(fs.readFileSync(path.resolve(__dirname,"../Data/productDataBase.json")));
         const ultimoProduct = productDataBase.pop();
         productDataBase.push(ultimoProduct);
+        
         const newProduct = {
 
-            id: ultProduct.id + 1,
+            id: ultimoProduct.id + 1,
             price : req.body.price,
             image: req.body.filename ,
             autor: req.body.autor,
@@ -24,11 +27,10 @@ const controller = {
 
         }
         productDataBase.push(newProduct);
-        const guardarProduct = JSON.stringify(productDataBase,null,2);
-        fs.writeFileSync(path.resolve(__dirname,'../Data/productDataBase.json'),
-        guardarProduct)
+        const guardarProduct = JSON.stringify(productDataBase,null, " ");
+        fs.writeFileSync(path.resolve(__dirname,'../Data/productDataBase.json'),guardarProduct);
 
-        res.redirect('/home');
+        res.redirect('/');
 
             
 
