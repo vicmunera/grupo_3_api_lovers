c69fd6de3fcc73dd96ab36ced50259d1e5805ee1
BraianMaynard <Braian_maynard@hotmail.com>
2022-07-18 20:35:39 -0300

    validation

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,13 +1,14 @@
-const express = require ("express");
+const express = require("express");
 const path = require('path');
 const multer = require('multer');
 const router = express.Router();
+const { body } = require('express-validator');
 
-const cartController = require ("../controllers/cartControllers"); 
+const cartController = require("../controllers/cartControllers");
 const usersControllers = require("../controllers/usersControllers");
 const userControllersLM = require('../controllers/userControllersLM')
 
-// donde voy a guardar imagenes de los productos
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.resolve(__dirname, '../../public/images/usersImg'))
@@ -20,11 +21,23 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 
+//validaciones del register
+const validations = [
+    body('name').notEmpty().withMessage('Tienes que escribir un nombre'),
+    body('surname').notEmpty().withMessage('Tienes que escribir un apellido'),
+    body('userName').notEmpty().withMessage('Tienes que escribir un nombre de usuario'),
+    body('email').notEmpty().withMessage('Tienes que escribir un email valido'),
+    body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+    body('date').notEmpty().withMessage('Tienes que escribir tu fecha de cumpleaños'),
+
+]
+
+
 router.get("/productCart", cartController.cart);
 router.get("/register", usersControllers.index);
-router.post("/register", upload.single('image'), usersControllers.create);
-router.get("/register", userControllersLM.processRegister);
-router.get("/register", userControllersLM.loginProcess);
+router.post('/register', validations, usersControllers.processRegister);
+router.post("/register", upload.single('image'), validations, usersControllers.create);
+
 
 
 
